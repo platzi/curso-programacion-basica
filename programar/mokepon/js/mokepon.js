e9619b2ea294952ad37d504a7c9c50a6e7a55c60
Juan David Castro <me@juandc.co>
2022-07-19 13:31:45 -0500

    Create js/mokepon.js

---
+++ programar/mokepon/js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador() {
+    alert('SELECCIONASTE TU MASCOTA')
+}
+
+let botonMascotaJugador = document.getElementById('boton-mascota')
+botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
