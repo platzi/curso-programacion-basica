01b0fe88964aa22b32f62ca52328f1d2ed2f6dfa
Juan David Castro <me@juandc.co>
2022-07-19 13:35:01 -0500

    Escuchando evento load

--- programar/mokepon/js/mokepon.js
+++ programar/mokepon/js/mokepon.js
@@ -1,6 +1,10 @@
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
 function seleccionarMascotaJugador() {
     alert('SELECCIONASTE TU MASCOTA')
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+window.addEventListener('load', iniciarJuego)
