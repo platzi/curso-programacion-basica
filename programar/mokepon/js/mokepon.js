6309496a6c3e88be6b86322b0f66896f34df3019
Juan David Castro <me@juandc.co>
2022-07-19 13:44:01 -0500

    Identificando mascota seleccionada

--- programar/mokepon/js/mokepon.js
+++ programar/mokepon/js/mokepon.js
@@ -4,7 +4,19 @@ function iniciarJuego() {
 }
 
 function seleccionarMascotaJugador() {
-    alert('SELECCIONASTE TU MASCOTA')
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    
+    if (inputHipodoge.checked) {
+        alert('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked) {
+        alert('Seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked) {
+        alert('Seleccionaste a Ratigueya')
+    } else {
+        alert('Selecciona una mascota')
+    }
 }
 
 window.addEventListener('load', iniciarJuego)
