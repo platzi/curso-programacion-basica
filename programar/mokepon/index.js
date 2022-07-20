106dedb5c4080a202233d6b6d13fa5f67606adeb
Juan David Castro <me@juandc.co>
2022-07-20 18:13:18 -0500

    JSON y POST

--- programar/mokepon/index.js
+++ programar/mokepon/index.js
@@ -1,12 +1,26 @@
 const express = require("express")
+const cors = require("cors")
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 const jugadores = []
 
 class Jugador {
   constructor(id) {
     this.id = id
+  }
+
+  asignarMokepon(mokepon) {
+    this.mokepon = mokepon
+  }
+}
+
+class Mokepon {
+  constructor(nombre) {
+    this.nombre = nombre
   }
 }
 
@@ -20,6 +34,22 @@ app.get("/unirse", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
   
   res.send(id)
+})
+
+app.post("mokepon/:jugadorId", (req, res) => {
+  const jugadorId = req.params.jugadorId || ""
+  const nombre = req.body.mokepon || ""
+  const mokepon = new Mokepon(nombre)
+  
+  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+
+  if (jugadorIndex >= 0) {
+    jugadores[jugadorIndex].asignarMokepon(mokepon)
+  }
+  
+  console.log(jugadores)
+  console.log(jugadorId)
+  res.end()
 })
 
 app.listen(8080, () => {
--- programar/mokepon/js/mokepon.js
+++ programar/mokepon/js/mokepon.js
@@ -21,6 +21,7 @@ const contenedorAtaques = document.getElementById('contenedorAtaques')
 const sectionVerMapa = document.getElementById('ver-mapa')
 const mapa = document.getElementById('mapa')
 
+let jugadorId = null
 let mokepones = []
 let ataqueJugador =[]
 let ataqueEnemigo = []
@@ -186,6 +187,7 @@ function unirseAlJuego() {
                 res.text()
                     .then(function (respuesta) {
                         console.log(respuesta)
+                        jugadorId = respuesta
                     })
             }
         })
@@ -208,9 +210,23 @@ function seleccionarMascotaJugador() {
         alert('Selecciona una mascota')
     }
 
+    seleccionarMokepon(mascotaJugador)
+    
     extraerAtaques(mascotaJugador)
     sectionVerMapa.style.display = 'flex'
     iniciarMapa()
+}
+
+function seleccionarMokepon(mascotaJugador) {
+    fetch(`http://localhost:8080/mokepon/${jugadorId}`, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            mokepon: mascotaJugador
+        })
+    })
 }
 
 function extraerAtaques(mascotaJugador) {
