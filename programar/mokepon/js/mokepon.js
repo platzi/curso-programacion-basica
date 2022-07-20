3e58cb53f74f1a890d63f9ca502f1aaeecfc7c65
Juan David Castro <me@juandc.co>
2022-07-20 11:12:12 -0500

    Movimiento hacia todas las direcciones

--- programar/mokepon/js/mokepon.js
+++ programar/mokepon/js/mokepon.js
@@ -42,6 +42,7 @@ let victoriasEnemigo = 0
 let vidasJugador = 3
 let vidasEnemigo = 3
 let lienzo = mapa.getContext("2d")
+let intervalo
 
 class Mokepon {
     constructor(nombre, foto, vida) {
@@ -55,6 +56,8 @@ class Mokepon {
         this.alto = 80
         this.mapaFoto = new Image()
         this.mapaFoto.src = foto
+        this.velocidadX = 0
+        this.velocidadY = 0
     }
 }
 
@@ -128,6 +131,7 @@ function seleccionarMascotaJugador() {
     
     //sectionSeleccionarAtaque.style.display = 'flex'
     sectionVerMapa.style.display = 'flex'
+    intervalo = setInterval(pintarPersonaje, 50)
     
     
     
@@ -308,6 +312,8 @@ function aleatorio(min, max) {
 }
 
 function pintarPersonaje() {
+    capipepo.x = capipepo.x + capipepo.velocidadX
+    capipepo.y = capipepo.y + capipepo.velocidadY
     lienzo.clearRect(0, 0, mapa.width, mapa.height)
     lienzo.drawImage(
         capipepo.mapaFoto,
@@ -318,9 +324,25 @@ function pintarPersonaje() {
     )
 }
 
-function moverCapipepo() {
-    capipepo.x = capipepo.x + 5
-    pintarPersonaje()
+function moverDerecha() {
+    capipepo.velocidadX = 5
+}
+
+function moverIzquierda() {
+    capipepo.velocidadX = -5
+}
+
+function moverAbajo() {
+    capipepo.velocidadY = 5
+}
+
+function moverArriba() {
+    capipepo.velocidadY = -5
+}
+
+function detenerMovimiento() {
+    capipepo.velocidadX = 0
+    capipepo.velocidadY = 0
 }
 
 window.addEventListener('load', iniciarJuego)
