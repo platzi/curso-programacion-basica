304ad22677c79f5a832bf90a6a7402bccb6f434a
Juan David Castro <me@juandc.co>
2022-07-20 08:11:13 -0500

    layout: title & ataques

--- programar/mokepon/js/mokepon.js
+++ programar/mokepon/js/mokepon.js
@@ -29,7 +29,7 @@ function seleccionarMascotaJugador() {
     sectionSeleccionarMascota.style.display = 'none'
     
     let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
-    sectionSeleccionarAtaque.style.display = 'block'
+    sectionSeleccionarAtaque.style.display = 'flex'
     
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
